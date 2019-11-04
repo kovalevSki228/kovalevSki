be9092ba1e082bf45cd25a28c046f5da436aa31c
kovalevski <39824896+kovalevSki228@users.noreply.github.com>
2019-11-04 05:32:13 +0300

    coments

--- SaitCourses/Models/CommentsHub.cs
+++ SaitCourses/Models/CommentsHub.cs
@@ -13,79 +13,11 @@ namespace SaitCourses.Models
         {
             _db = context;
         }
-        public async Task Send(string message, string userName, string tShirt)
+        public async Task Send(string message, string userName)
         {
-            User user = _db.Users.FirstOrDefault(item => item.UserName == userName);
-            Comment comment = _db.comments.FirstOrDefault(item => item.tShirtId + "" == tShirt && item.user == user);
-            if (comment == null)
-            {
-                if (message != "")
-                {
-                    _db.comments.Add(new Comment { user = user,userId = user.Id,  tShirtId = Convert.ToInt32(tShirt), text = message });
-                 //   await _db.SaveChangesAsync();
-                }
-            }
-            else
-            {
-                if (message == "")
-                {
-                    _db.comments.Remove(comment);
-                    _db.SaveChanges();
-                }
-                else
-                {
-                    comment.text = message;
-                    _db.comments.Update(comment);
-                    _db.SaveChanges();
-                }
-            }
-            comment = _db.comments.FirstOrDefault(item => item.tShirtId + "" == tShirt && item.user == user);
-            if (comment != null)
-                await Clients.All.SendAsync("Send", message, userName, comment.id);
-            else
-                await Clients.All.SendAsync("Send", message, userName, 0);
+
+            await Clients.All.SendAsync("Send", message, userName);
         }
-        public async Task Rating(string userName, bool value, string tShirtId)
-        {
-            int changeValue = 0;
-            Comment comment = _db.comments.FirstOrDefault(item => item.id + "" == tShirtId);
-            User user = _db.Users.FirstOrDefault(item => item.UserName == userName);
-            Like rating = _db.likes.FirstOrDefault(item => item.shirtId == Int32.Parse(tShirtId) && item.userId == user.Id);
-            int rat;
-            if(value)
-            {
-                rat = 1;
-            }
-            else
-            {
-                rat = -1;
-            }
-            if (rating == null)
-            {
-                _db.likes.Add(new Like { userId = user.Id, shirtId = Int32.Parse(tShirtId), like = rat });
-                _db.SaveChanges();
-                if (value) changeValue = 1;
-                else changeValue = -1;
-            }
-            else
-            {
-                if (rating.like == 1)
-                {
-                    _db.likes.Remove(rating);
-                    _db.SaveChanges();
-                    if (value) changeValue = -1;
-                    else changeValue = 1;
-                }
-                else
-                {
-                    rating.like = 1;
-                    _db.likes.Update(rating);
-                    _db.SaveChanges();
-                    if (value) changeValue = 2;
-                    else changeValue = -2;
-                }
-            }
-            await Clients.All.SendAsync("Rating", tShirtId, changeValue);
-        }
+        
     }
 }
--- SaitCourses/wwwroot/js/CommentHub.js
+++ SaitCourses/wwwroot/js/CommentHub.js
@@ -3,105 +3,22 @@ const hubConnection = new signalR.HubConnectionBuilder()
     .withUrl(hubUrl)
     .configureLogging(signalR.LogLevel.Information)
     .build();
-hubConnection.on('Send', function (message, userName, comment) {
-    let messages = document.querySelectorAll(".author");
-    let dublicate = false;
-    for (let i = 0; i < messages.length; i++) {
-        if (messages[i].textContent == userName) {
-            dublicate = true;
-            if (message == "")
-                messages[i].parentNode.remove();
-            else
-                messages[i].parentNode.querySelector(".text-context").textContent = message;
-        }
-    }
-    if (!dublicate) {
-        let container = CreateCommentBlock(message, userName, comment);
 
-        var firstElem = document.getElementById("comments-list").firstChild;
-        document.getElementById("comments-list").insertBefore(container, firstElem);
-    }
-
-});
-function CreateCommentBlock(message, userName, comment) {
-    let container = document.createElement("div");
-    container.classList.add("border");
-    container.classList.add("rounded");
-    container.classList.add("p-3");
-    container.classList.add("mb-3");
-
-    let userNameElem = document.createElement("h2");
-    userNameElem.appendChild(document.createTextNode(userName));
-    userNameElem.classList.add("author");
+hubConnection.on("Send", function (message, userName) {
 
     let elem = document.createElement("p");
-    elem.classList.add("text-context")
-    elem.appendChild(document.createTextNode(message));
-    let ratingSpan = document.createElement("span");
-    ratingSpan.appendChild(document.createTextNode("Rating: "));
-    let rating = document.createElement("span");
-    rating.classList.add("rating");
-    rating.appendChild(document.createTextNode("0"));
-    let like = document.createElement("a");
-    like.classList.add("like");
-    let likeImg = document.createElement("img");
-    likeImg.src = "/img/like.png";
-    likeImg.width = "18";
-    like.appendChild(likeImg);
-    like.classList.add("ml-3");
-    let dislike = document.createElement("a");
-    dislike.classList.add("dislike");
-    let dislikeImg = document.createElement("img");
-    dislikeImg.src = "/img/dislike.png";
-    dislikeImg.width = "18";
-    dislike.appendChild(dislikeImg);
-    dislike.classList.add("ml-3");
-    let commentId = document.createElement("input");
-    commentId.classList.add("comment-id");
-    commentId.type = "hidden";
-    commentId.value = comment;
+    let div = document.createElement("div");
+    div.innerHTML = " <div class=\"border rounded p - 3 mb - 3\"><h2 class=\"author\" >" + userName + "</h2 ><p class=\"text-context\">" + message + "</p><span>Rating: </span><span class=\"rating\">Лайки</span><a class=\"like ml-3\"><img src=\"https://res.cloudinary.com/del5wrr12/image/upload/v1572829492/w2gh8giame3qj8q8smjp.png \" width=\"18\" /></a><a class=\"dislike ml-3\"><img src=\"https://res.cloudinary.com/del5wrr12/image/upload/v1572829492/veslqx4w2mepqah3br1q.png \" width=\"18\" /></a><input type=\"hidden\" class=\"comment-id\" value=\"Text\" /></div >";
 
-    container.appendChild(userNameElem);
-    container.appendChild(elem);
-    container.appendChild(ratingSpan);
-    container.appendChild(rating);
-    container.appendChild(like);
-    container.appendChild(dislike);
-    container.appendChild(commentId);
-    like.addEventListener("click", likeListener);
-    dislike.addEventListener("click", dislikeListener);
-    return container;
-}
-hubConnection.on('Rating', function (commentId, changeValue) {
-    let comments = document.querySelectorAll(".comment-id");
-    for (let i = 0; i < comments.length; i++) {
-        if (comments[i].value == commentId)
-            comments[i].parentNode.querySelector(".rating").textContent = parseInt(comments[i].parentNode.querySelector(".rating").textContent) + changeValue;
-    }
+    elem.appendChild(div);
+    let firstElem = document.getElementById("chatroom").lastChild;
+    document.getElementById("chatroom").insertBefore(elem, firstElem);
+
 });
+
 document.getElementById("sendBtn").addEventListener("click", function (e) {
-    let message = document.getElementById("comment").value;
-    hubConnection.invoke('Send', message.toString(), userName, shirt);
+    let message = document.getElementById("message").value;
+    hubConnection.invoke("Send", message, userName);
 });
-document.addEventListener("keydown", function (event) {
-    if (event.code == "Enter") {
-        let message = document.getElementById("comment").value;
-        hubConnection.invoke('Send', message, userName, shirt);
-    }
-});
-let likes = document.querySelectorAll(".like");
-let dislikes = document.querySelectorAll(".dislike");
-let likeListener = function (e) {
-    let comment = e.currentTarget.parentNode.querySelector(".comment-id").value;
-    hubConnection.invoke('Rating', userName, true, comment);
-}
-let dislikeListener = function (e) {
-    let comment = e.currentTarget.parentNode.querySelector(".comment-id").value;
-    hubConnection.invoke('Rating', userName, false, comment);
-}
-for (let i = 0; i < likes.length; i++) {
-    likes[i].addEventListener("click", likeListener);
-    dislikes[i].addEventListener("click", dislikeListener);
-}
 
 hubConnection.start();
